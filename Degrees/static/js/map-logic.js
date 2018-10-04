7100d8859e85b21dc82efa028eb473de10ca132c
monica-t-james <36522398+shinergrrl@users.noreply.github.com>
2018-10-03 23:12:40 -0500

    Updated geo-map to include all years

--- Degrees/static/js/map-logic.js
+++ Degrees/static/js/map-logic.js
@@ -1,22 +1,22 @@
 // Creating map object
-var myMap = L.map("map", {
+var map = L.map("map", {
   center: [37.09, -95.71],
   zoom: 5
 });
 const yearLayer = new L.LayerGroup();
-yearLayer.addTo(myMap)
+yearLayer.addTo(map)
 // Adding tile layer
 L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
   attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   maxZoom: 18,
   id: "mapbox.streets",
   accessToken: API_KEY
-}).addTo(myMap);
+}).addTo(map);
 
 // Link to JSON files
 var link1 = "https://raw.githubusercontent.com/PublicaMundi/MappingAPI/master/data/geojson/us-states.json";
-var link2 = "https://raw.githubusercontent.com/RCate/Project_2/master/Resources/Tuition/tuitionPercent.json";
-var link3 = "https://raw.githubusercontent.com/RCate/Project_2/master/4YrEnrollPctChange.json"
+var link2 = "https://raw.githubusercontent.com/monica-t-james/Project_3/master/Degrees/Resources/Tuition/tuitionPercent.json";
+var link3 = "https://raw.githubusercontent.com/monica-t-james/Project_3/master/Degrees/Resources/4YrEnrollPctChange.json"
 
 d3.queue()
   .defer(d3.json, link1)
@@ -46,9 +46,6 @@ function ready(error, mapData, csvData, csvData2) {
     feature.enrollment = csvData2_obj[feature.properties.name]
   })
 
-  
-  // var chosenYear = "Y2009"
-   
   filterYear("Y2009")
 
   function filterYear(chosenYear) {
@@ -88,16 +85,17 @@ function ready(error, mapData, csvData, csvData2) {
           d >= .02 ? '#f0cc3d' :
           d >= .01 ? '#aadb12' :
           d >= 0 ? '#8cb709' :
-                    'blue';
+          d < 0 ? 'blue':
+                  'black';
   }
 
     var legend = L.control({position: 'bottomright'});
 
-    legend.onAdd = function (myMap) {
+    legend.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend'),
-        grades = [0, .01, .02, .03, .04, .05],
-        labels = ["0%", "1%", "2%", "3%", "4%", "5%"];
+        grades = [-1, 0, .01, .02, .03, .04, .05],
+        labels = ["less than", "0%", "1%", "2%", "3%", "4%", "5%"];
 
     // loop through our density intervals and generate a label with a colored square for each interval
     for (var i = 0; i < grades.length; i++) {
@@ -109,21 +107,26 @@ function ready(error, mapData, csvData, csvData2) {
     return div;
     }
     // Adding legend to the map   
-    legend.addTo(myMap);
-
-
+    legend.addTo(map);
     var dropdown = L.control({position: 'topright'});
-
-    dropdown.onAdd = function (myMap) {
+    dropdown.onAdd = function (map) {
 
     var div = L.DomUtil.create('div', 'info legend');
-    div.innerHTML = '<select><option value="Y2009">2009</option><option value="Y2015">2015</option>';
+    div.innerHTML = 
+      '<select> \
+      <option value="Y2009">2009</option> \
+      <option value="Y2010">2010</option> \
+      <option value="Y2011">2011</option> \
+      <option value="Y2012">2012</option> \
+      <option value="Y2013">2013</option> \
+      <option value="Y2014">2014</option> \
+      <option value="Y2015">2015</option>';
     div.firstChild.onmousedown = div.firstChild.ondblclick = L.DomEvent.stopPropagation;
     return div;
 
     }
     // Adding legend to the map   
-    dropdown.addTo(myMap);
+    dropdown.addTo(map);
     dropdown._container.onchange = function() {
       console.log()
       filterYear(event.target.value)
